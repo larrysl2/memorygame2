3980e953a3de551abd4a992b9ecc3bd6f8e02c57
Larry Lau <lsl1840@gmail.com>
2022-12-24 15:29:18 -0600

    setup gh-pages

--- src/components/cardlist.js
+++ src/components/cardlist.js
@@ -1,27 +1,43 @@
+import cr from "../img/memorygame/cr.jpeg";
+import ds from "../img/memorygame/ds.jpeg";
+import eH from "../img/memorygame/eH.jpg";
+import KDB from "../img/memorygame/KDB.jpeg";
+import LM from "../img/memorygame/LM.jpeg";
+import MS from "../img/memorygame/MS.jpeg";
+import neymar from "../img/memorygame/neymar.jpg";
+import PP from "../img/memorygame/PP.jpeg";
 const cardlist = [
     {name: "Paul Pogba",
-    clicked:false
+    clicked:false,
+    img: PP
     },
     {name: "Kevin De Bruyne",
-    clicked:false
+    clicked:false,
+    img: KDB
     },
     {name: "Erling Haaland",
-    clicked:false
+    clicked:false,
+    img: eH
     },
     {name: "David Silva",
-    clicked:false
+    clicked:false,
+    img: ds
     },
     {name: "Cristiano Ronaldo",
-    clicked:false
+    clicked:false,
+    img:cr
     },
     {name: "Neymar Jr",
-    clicked:false
+    clicked:false,
+    img: neymar
     },
     {name: "Lionel Messi",
-    clicked:false
+    clicked:false,
+    img: LM
     },
     {name: "Mohammed Salah",
-    clicked:false
+    clicked:false,
+    img: MS
     }
 ];
 export default cardlist;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,30 +1,44 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import cardlist from './cardlist';
+import '../style.css'
 
 const Layout = () => {
     const [score, setScore] = useState(0);
     const [hiscore, setHiscore] = useState(0);
-    const [list, setList] = useState(cardlist);
-    console.log(cardlist);
+    let [list, setList] = useState(cardlist);
+    
+    useEffect(()=>{if (score==8){
+        setHiscore(score);
+        alert("YOU WIN! Click ok to restart.");
+        setScore(0);
+        var list2 = list;
+        list2.map((x)=>{
+            x.clicked=false;
+        })
+        list = list2;
+    }
+    else if (score>hiscore){
+        setHiscore(score);
+    }});
+    
     const scoreupdate =(x)=>{
         if (x.clicked==false){
             setScore(score + 1);
-            if (score>=hiscore){
-                setHiscore(score+1);
+            x.clicked=true;
             }
-        }
-        else{
-            setScore(0);
-            var list2 = list;
-            list2.map((x)=>{
-                x.clicked=false;
-            })
-            list = list2;
-        }
+            else{
+                alert("You already clicked that card! Click ok to restart.")
+                setScore(0);
+                var list2 = list;
+                
+                list2.map((x)=>{
+                    x.clicked=false;
+                })
+                setList(list2);
+            }
         }
         function shuffleArray(array1,x,scoreupdate) {
             scoreupdate(x);
-            x.clicked=true;
             let array = [...array1];
             for (var i = array.length - 1; i > 0; i--) {
                 var j = Math.floor(Math.random() * (i + 1));
@@ -37,15 +51,20 @@ const Layout = () => {
         }
         
     return(
-        <div>
-            {"Current Score:" + score} {"High Score:"+ hiscore}
-            <div className="cardname">{list.map((x) => {
-            return (<div >
-                    <div className = "cardname" key={x.name} onClick={()=>shuffleArray(cardlist,x,scoreupdate)}>{x.name} </div>
+        <div className="page">
+            <div className="header">
+                <div className="gamename">Memory Card Game</div>
+                <div className="scoreboard">{"Current Score:" + score} {"High Score:"+ hiscore}</div>
+            </div>
+            <div className="instructs">Click on each card once to earn a point!</div>
+            <div className="cardbox">{list.map((x) => {
+            return (<div className="imageholder">
+                        <div className = "cardname" key={x.name} onClick={()=>shuffleArray(cardlist,x,scoreupdate)}> {x.name} </div>
+                        <img className = 'image' src ={x.img} onClick={()=>shuffleArray(cardlist,x,scoreupdate)}></img>
                     </div>);
-        })}</div>
+            })}</div>
         </div>
         );
-    }
+}
 export default Layout;
    
